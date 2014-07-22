a22bfbde417faeccad4ddab0ae41978f41d02f00
cesc <mshao@splunk.com>
2014-07-22 11:30:59 +0800

    add track for the scripts

--- aws/deploy_splunk.sh
+++ aws/deploy_splunk.sh
@@ -3,8 +3,8 @@
 SPLUNK_RELEASE="http://ec2-54-179-189-159.ap-southeast-1.compute.amazonaws.com/splunk-6.1.2-213098-Linux-x86_64.tgz"
 DEPLOY_RUN_FILE="deploy_aws_$$.sh"
 
-cat "../../amazone_server_config" | while read line; do
-	host=`echo $line|cut -d " " -f2`
+cat "./aws_instance_list" | while read line; do
+	host=`echo $line|cut -d "," -f2`
 	echo $host
 	echo "sh ../sh/setup-instance.sh $host $SPLUNK_RELEASE splunk" >> $DEPLOY_RUN_FILE
 done
--- aws/deploy_splunk_cluster.sh
+++ aws/deploy_splunk_cluster.sh
@@ -3,10 +3,29 @@
 SPLUNK_RELEASE="http://ec2-54-179-189-159.ap-southeast-1.compute.amazonaws.com/splunk-6.1.2-213098-Linux-x86_64.tgz"
 DEPLOY_RUN_FILE="deploy_aws_cluster_$$.sh"
 
-cat "../../amazone_server_config" | while read line; do
-	host=`echo $line|cut -d " " -f2`
-	nodeType=`echo $line|cut -d " " -f3`
-	echo "sh ../sh/cluster/setup-cluster-node.sh $nodeType $host splunk" >> $DEPLOY_RUN_FILE
+export LICENSE_MASTER
+export MASTER
+while read line; do
+	host=`echo $line|cut -d "," -f2`
+	nodeType=`echo $line|cut -d "," -f3`
+	if [ $nodeType == "licenseMaster" ]; then
+		LICENSE_MASTER=$host
+	elif [ $nodeType == "master" ]; then
+		MASTER=$host
+	fi
+done<"./aws_instance_list"
+
+echo $LICENSE_MASTER
+echo $MASTER
+
+cat "./aws_instance_list" | while read line; do
+	host=`echo $line|cut -d "," -f2`
+	nodeType=`echo $line|cut -d "," -f3`
+	if [ $nodeType == "master" ]; then
+		echo "sh ../sh/cluster/setup-cluster-node.sh $nodeType $host splunk $LICENSE_MASTER $MASTER" >> $DEPLOY_RUN_FILE
+	elif [ $nodeType == "searchHead" ] || [ $nodeType == "slave" ]; then
+		echo "sh ../sh/cluster/setup-cluster-node.sh $nodeType $host splunk $LICENSE_MASTER $MASTER" >> $DEPLOY_RUN_FILE
+	fi
 done
 
 chmod +x $DEPLOY_RUN_FILE
